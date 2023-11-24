9548abce3bf5138dd7b57c1acd6c326d27c8e6bc
Phuc <hoangphucythus@gmail.com>
2023-11-24 08:05:16 +0700

    manage book

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import List from './components/List'
-import Login from './components/Login';
+import manageBook from './components/managebook';
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,7 +8,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route index element={<Login />} />
+        <Route index element={<manageBook />} />
       
       </Routes>
     </BrowserRouter>
